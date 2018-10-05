71aca704510789dea6f3f0b014d07ccddfc48ebf
Oyewo Ayooluwa <ayooluwaoyewo@gmail.com>
2018-10-05 15:14:44 +0100

    side navigation created

--- js/main.js
+++ js/main.js
@@ -1,33 +1,7 @@
+$(document).ready(function () {
 
-// change navbar color on scroll
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
 });
-
-// change navitem color on scroll
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".nav-item");
-      $nav.toggleClass('scroll', $(this).scrollTop() > $nav.height());
-    });
-  });
-
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar-brand>span");
-      $nav.toggleClass('scrolling', $(this).scrollTop() > $nav.height());
-    });
-  });
-// $(window).on('scroll', function () {
-// if ($(this).scrollTop() > $nav.height()) { // Set position from top
-    
-//     $('.navbar-brand  img').attr('src', 'images/logo.png');
-// }else{
-    
-//     $('.navbar-brand  img').attr('src', 'images/logo.png');
-// }
-// });
-new WOW().init();
